c6d3a92592a92aea2623520c9178d318e8a1f207
quhongbin <you@example.com>
2016-02-29 13:55:14 +0800

    修改bug

--- example/testringbuffer.go
+++ example/testringbuffer.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/wowqhb/ringbuffer/ringbuffer"
 	"fmt"
+	"time"
+	"bytes"
 )
 
 func main() {
@@ -10,13 +12,47 @@ func main() {
 	rbuffer.RingBufferInit(int64(200))
 	fmt.Println(rbuffer.GetCurrentReadIndex())
 	fmt.Println(rbuffer.GetCurrentWriteIndex())
-	bytes := make([]byte, 20)
+	/*bytes := make([]byte, 20)
+	bytes[1] = byte(20)
 	fmt.Println(&bytes)
 	ok := rbuffer.WriteBuffer(&bytes)
 	fmt.Println(ok)
 	retP, ok := rbuffer.ReadBuffer()
 	fmt.Println(ok)
 	fmt.Println(retP)
-	fmt.Println(&retP)
+	bytes[0] = byte(19)
+	fmt.Println(bytes)
+	fmt.Println(retP)*/
+	go writegoroutine(&rbuffer)
+	go readgoroutine(&rbuffer)
 
+	time.Sleep(60 * time.Second)
+
+}
+
+func readgoroutine(rbuffer  *ringbuffer.RingBuffer) {
+	for {
+		retP, ok := rbuffer.ReadBuffer()
+		if ok {
+			fmt.Print("read::")
+			fmt.Print(retP)
+			fmt.Print(" =>> ")
+			fmt.Println(ok)
+		}else {
+			fmt.Print("read::nil =>> ")
+			fmt.Println(ok)
+		}
+		time.Sleep(10*time.Millisecond)
+	}
+}
+
+func writegoroutine(rbuffer *ringbuffer.RingBuffer) {
+	for {
+		time_ := time.Now().String()
+		bytes := bytes.NewBufferString(time_).Bytes()
+		ok := rbuffer.WriteBuffer(&bytes)
+		fmt.Print("write::" + time_ + " =>> ")
+		fmt.Println(ok)
+		time.Sleep(10*time.Millisecond)
+	}
 }
