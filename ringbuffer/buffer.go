869ebdea02177fde54364eee792c8db32a9c8e47
quhongbin <you@example.com>
2016-02-29 10:48:50 +0800

    加入ringbuffer代码

---
+++ ringbuffer/buffer.go
@@ -0,0 +1,75 @@
+package ringbuffer
+
+import (
+	"sync/atomic"
+)
+
+type Ringbuffer struct {
+	readIndex  uint64    //读序号
+	writeIndex uint64    //写序号
+	ringBuffer []*[]byte //环形buffer指针数组
+	bufferSize uint64    //初始化环形buffer指针数组大小
+}
+
+/**
+初始化ringbuffer
+参数bufferSize：初始化环形buffer指针数组大小
+ */
+func (buffer *Ringbuffer)RingBufferInit(bufferSize uint64) {
+	buffer.readIndex = 0
+	buffer.writeIndex = 0
+	buffer.bufferSize = bufferSize
+	buffer.ringBuffer = make([]*[]byte, buffer.bufferSize)
+}
+
+/**
+获取当前读序号
+ */
+func (buffer *Ringbuffer)GetCurrentReadIndex() (uint64) {
+	return buffer.readIndex
+}
+/**
+获取当前写序号
+ */
+func (buffer *Ringbuffer)GetCurrentWriteIndex() (uint64) {
+	return buffer.writeIndex
+}
+
+/**
+读取ringbuffer指定的buffer指针，返回该指针并清空ringbuffer该位置存在的指针内容，以及将读序号加1
+ */
+func (buffer *Ringbuffer)ReadBuffer(index uint64) (p *[]byte, ok error) {
+	ok = true
+	p = nil
+	switch  {
+	case buffer.readIndex > buffer.writeIndex:
+		ok = false
+	case buffer.writeIndex - buffer.readIndex > buffer.bufferSize:
+		ok = false
+	default:
+		p = &(buffer.ringBuffer[index])
+		buffer.ringBuffer[index] = nil
+		buffer.readIndex = atomic.AddUint64(&buffer.readIndex, 1)
+	}
+	return p, ok
+}
+
+/**
+写入ringbuffer指针，以及将写序号加1
+ */
+func (buffer *Ringbuffer)WriteBuffer(in *[]byte) (ok error) {
+	ok = true
+	switch  {
+	case buffer.writeIndex - buffer.readIndex >= buffer.bufferSize:
+		ok = false
+	default:
+		index := atomic.AddUint64(&buffer.writeIndex, 1)
+		if buffer.ringBuffer[index] == nil {
+			buffer.ringBuffer[index] = in
+			buffer.writeIndex = index
+		}else {
+			ok = false
+		}
+	}
+	return ok
+}
