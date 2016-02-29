e81fd4b15f97ecf683c44dad58f63f5759e26ecf
quhongbin <you@example.com>
2016-02-29 12:07:26 +0800

    修改bug

--- example/testringbuffer.go
+++ example/testringbuffer.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"github.com/wowqhb/ringbuffer/ringbuffer"
+	"fmt"
+)
 
 func main() {
-
+	rbuffer := ringbuffer.RingBuffer{}
+	rbuffer.RingBufferInit(int64(200))
+	fmt.Println(rbuffer.GetCurrentReadIndex())
+	fmt.Println(rbuffer.GetCurrentWriteIndex())
 }
