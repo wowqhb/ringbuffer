d2acaa6adcfd5c9f2298b73bf292214c5f401a75
wowqhb <13821056120@139.com>
2016-03-17 15:49:36 +0800

    修改ringbuffer锁

--- ringbuffer/RingBuffer.go
+++ ringbuffer/RingBuffer.go
@@ -63,13 +63,11 @@ func (this *RingBuffer) GetCurrentWriteIndex() int64 {
 读取ringbuffer指定的buffer指针，返回该指针并清空ringbuffer该位置存在的指针内容，以及将读序号加1
 */
 func (this *RingBuffer) ReadBuffer() (p *[]byte, ok bool) {
-	/*this.ccond.L.Lock()
+	this.ccond.L.Lock()
 	defer func() {
-		//this.pcond.Signal()
-		//this.pcond.Broadcast()
+		this.pcond.Signal()
 		this.ccond.L.Unlock()
-		//time.Sleep(3 * time.Millisecond)
-	}()*/
+	}()
 	ok = false
 	p = nil
 	readIndex := this.GetCurrentReadIndex()
@@ -80,24 +78,15 @@ func (this *RingBuffer) ReadBuffer() (p *[]byte, ok bool) {
 		}
 		writeIndex = this.GetCurrentWriteIndex()
 		if readIndex >= writeIndex {
-			this.ccond.L.Lock()
 			this.pcond.Signal()
-			//this.pcond.Broadcast()
 			this.ccond.Wait()
-			this.ccond.L.Unlock()
-			//runtime.Gosched()
-			//time.Sleep(5 * time.Millisecond)
 		} else {
 			break
 		}
-		//time.Sleep(1 * time.Millisecond)
-		//time.Sleep(500 * time.Microsecond)
 	}
 	index := readIndex & this.mask //替代求模
-	this.ccond.L.Lock()
 	p = this.buf[index]
 	this.buf[index] = nil
-	this.ccond.L.Unlock()
 	atomic.AddInt64(&this.readIndex, int64(1))
 	if p != nil {
 		ok = true
@@ -109,13 +98,11 @@ func (this *RingBuffer) ReadBuffer() (p *[]byte, ok bool) {
 写入ringbuffer指针，以及将写序号加1
 */
 func (this *RingBuffer) WriteBuffer(in *[]byte) (ok bool) {
-	/*this.pcond.L.Lock()
+	this.pcond.L.Lock()
 	defer func() {
-		//this.ccond.Signal()
-		//this.ccond.Broadcast()
+		this.ccond.Signal()
 		this.pcond.L.Unlock()
-		//time.Sleep(3 * time.Millisecond)
-	}()*/
+	}()
 	ok = false
 	readIndex := this.GetCurrentReadIndex()
 	writeIndex := this.GetCurrentWriteIndex()
@@ -125,23 +112,14 @@ func (this *RingBuffer) WriteBuffer(in *[]byte) (ok bool) {
 		}
 		readIndex = this.GetCurrentReadIndex()
 		if writeIndex >= readIndex && writeIndex-readIndex >= this.bufSize {
-			this.pcond.L.Lock()
 			this.ccond.Signal()
-			//this.ccond.Broadcast()
 			this.pcond.Wait()
-			this.pcond.L.Unlock()
-			//runtime.Gosched()
-			//time.Sleep(5 * time.Millisecond)
 		} else {
 			break
 		}
-		//time.Sleep(1 * time.Millisecond)
-		//time.Sleep(500 * time.Microsecond)
 	}
 	index := writeIndex & this.mask //替代求模
-	this.pcond.L.Lock()
 	this.buf[index] = in
-	this.pcond.L.Unlock()
 	atomic.AddInt64(&this.writeIndex, int64(1))
 	ok = true
 	return ok
@@ -152,12 +130,10 @@ func (this *RingBuffer) Close() error {
 
 	this.pcond.L.Lock()
 	this.ccond.Signal()
-	//this.ccond.Broadcast()
 	this.pcond.L.Unlock()
 
 	this.ccond.L.Lock()
 	this.pcond.Signal()
-	//this.pcond.Broadcast()
 	this.ccond.L.Unlock()
 
 	return nil
