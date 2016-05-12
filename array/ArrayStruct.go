f656ff069830bd4466122f09d6e87f8b1f659762
axe <13821056129@139.com>
2016-05-12 16:46:02 +0800

    修改为channel

---
+++ array/ArrayStruct.go
@@ -0,0 +1,15 @@
+package array
+
+var ArrayStruct struct {
+	p       []byte
+	realLen int64
+	maxLen  int64
+}
+
+func (this *ArrayStruct) NewArrayStruct() *ArrayStruct {
+	return ArrayStruct{
+		p:       make([]byte, 1024),
+		realLen: int64(0),
+		maxLen:  int64(1024),
+	}
+}
